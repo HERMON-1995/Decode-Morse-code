d8f49bb7d653c185047f7d00b7e901b95a634a2b
Thiri <thirihtet246@gmail.com>
2023-05-10 22:04:57 +0630

    Add linters

---
+++ decode_morse.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+MORSE_CODE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char]
+end
+
+def decode_word(morse_word)
+  morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
+end
+
+def decode(morse_message)
+  morse_message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+end
